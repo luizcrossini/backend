c691638de2ed122e50e6ffcd47004fde06d1d16b
luizcrossini <114169071+luizcrossini@users.noreply.github.com>
2026-01-14 20:43:54 -0300

    fix express namespace types323

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://www.luizrossini.com.br',
+    origin: [
+      'http://luizrossini.com.br',
+      'https://luizrossini.com.br',
+      'http://www.luizrossini.com.br',
+      'https://www.luizrossini.com.br',
+    ],
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
