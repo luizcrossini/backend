772d279df85cf153f2fde79d022dc4af5268fc05
luizcrossini <114169071+luizcrossini@users.noreply.github.com>
2026-01-26 17:18:06 -0300

    fix cors: allow https origin

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://www.luizrossini.com.br','https://www.luizrossini.com.br'],
+    origin: [
+      'http://www.luizrossini.com.br',
+      'https://www.luizrossini.com.br',
+      'https://luizrossini.com.br',
+    ],
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
